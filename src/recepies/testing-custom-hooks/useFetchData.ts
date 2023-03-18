5889c519b5725014dd0e881c98b63475b45f89e1
Talwinder Singh <singhtalwinder790@gmail.com>
2023-03-18 10:45:12 +0530

    feat: add useFetchData hook

---
+++ src/recepies/testing-custom-hooks/useFetchData.ts
@@ -0,0 +1,32 @@
+import { useState, useEffect } from 'react';
+
+const useFetchData = (url: string) => {
+  const [data, setData] = useState<any>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<Error | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const jsonData = await response.json();
+        setData(jsonData);
+        setError(null);
+      } catch (error) {
+        setError(error as Error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [url]);
+
+  return { data, loading, error };
+};
+
+export default useFetchData;
