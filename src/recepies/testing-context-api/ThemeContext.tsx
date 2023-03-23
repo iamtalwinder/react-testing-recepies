0f6637c930f45d88d733fee36e9a50e263ac6df3
Talwinder Singh <singhtalwinder790@gmail.com>
2023-03-23 12:38:33 +0530

    feat: add ThemeContext

---
+++ src/recepies/testing-context-api/ThemeContext.tsx
@@ -0,0 +1,30 @@
+import React, { createContext, useState, useContext, ReactNode } from 'react';
+
+type ThemeContextType = {
+  theme: string;
+  toggleTheme: () => void;
+};
+
+const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
+
+export const useTheme = (): ThemeContextType => {
+  const context = useContext(ThemeContext);
+  if (!context) {
+    throw new Error('useTheme must be used within a ThemeProvider');
+  }
+  return context;
+};
+
+export const ThemeProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
+  const [theme, setTheme] = useState<string>('light');
+
+  const toggleTheme = () => {
+    setTheme(theme === 'light' ? 'dark' : 'light');
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
