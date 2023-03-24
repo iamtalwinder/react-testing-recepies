5a60228c672fe4e10f854991210bf1268fcaa705
Talwinder Singh <singhtalwinder790@gmail.com>
2023-03-24 12:38:33 +0530

    feat: add ThemeSwitcher

---
+++ src/recepies/testing-context-api/ThemeSwitcher.tsx
@@ -0,0 +1,13 @@
+import { useTheme } from './ThemeContext';
+
+const ThemeSwitcher = () => {
+  const { theme, toggleTheme } = useTheme();
+
+  return (
+    <button onClick={toggleTheme}>
+      Switch to {theme === 'light' ? 'Dark' : 'Light'} Theme
+    </button>
+  );
+};
+
+export default ThemeSwitcher;
